8790e23e5a2426b7c85140f1173ad6a583714a7a
Daniel Kestler <daniel.kestler@medienreaktor.de>
2013-12-19 11:25:58 +0100

    Sort page columns by modified date

---
+++ filters/fau-admin.php
@@ -0,0 +1,9 @@
+<?php
+
+function set_post_order_in_admin( $wp_query ) {
+  if ( is_admin() ) {
+    $wp_query->set( 'orderby', 'modified' );
+    $wp_query->set( 'order', 'DESC' );
+  }
+}
+add_filter('pre_get_posts', 'set_post_order_in_admin' );
